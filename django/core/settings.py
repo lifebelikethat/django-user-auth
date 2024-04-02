789e9c6a253ed07d7f1484af4387cc0b0e85e54f
lifebelikethat <ianharoldong@yahoo.com>
2024-04-02 14:29:19 +0800

    modified csrf trusted origins in settings.py

--- django/core/settings.py
+++ django/core/settings.py
@@ -40,7 +40,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://django-user-auth-dev-hkqg.1.us-1.fl0.io']
+CSRF_TRUSTED_ORIGINS = ['https://django-user-auth-dev-hkqg']
 
 AUTH_USER_MODEL = 'auth.User'
 
